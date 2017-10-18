00972ef962045881e6b23f36f2d925453bff284d
andreip <andrei.popescu@infoworld.ro>
2017-10-18 11:24:10 +0300

    Setting up server

---
+++ server/server.js
@@ -0,0 +1,14 @@
+//into the package.json we define "start", telling heroku how to start the app
+//also we define "engines" telling heroku what version of node to use 
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;//for heroku
+
+const express = require('express');
+var app = express();
+//console.log(__dirname + '/../public');
+//console.log(publicPath);
+app.use(express.static(publicPath));
+app.listen(3000, ()=>{
+    console.log(`Server is up on port ${port}`);
+});
